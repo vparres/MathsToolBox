20458b4e68d627ab2c063c74dd52cf8f4ef3df1c
Vincent PARRES <vparres@vparres.com>
2014-12-24 19:16:14 +0100

    Version 0.4b : Calcul 3D et réorganisation
    Version 0.4b : Calcul 3D (programme de Terminale S), suppression des
    calculs avec les fonctions affines et réorganisation du menu.

--- Main.vb
+++ Main.vb
@@ -1,4 +1,6 @@
-﻿Module Main
+﻿'Importation des librairies
+Imports System.Math
+Module Main
 
     Sub Main()
         'Déclaration des variables
@@ -15,30 +17,39 @@
         Dim YB As Integer
         Dim YC As Integer
         Dim YD As Integer
+        Dim ZA As Integer
+        Dim ZB As Integer
+        Dim ZC As Integer
+        Dim ZD As Integer
         Dim XBA As Integer
         Dim YBA As Integer
+        Dim ZBA As Integer
         Dim XCB As Integer
         Dim YCB As Integer
+        Dim ZCB As Integer
+        Dim XAC As Integer
+        Dim YAC As Integer
+        Dim ZAC As Integer
 
         'Début du Programme
 Menu:
         Console.WriteLine("##############################################")
         Console.WriteLine("##          Maths ToolBox By VPARRES        ##")
-        Console.WriteLine("##            Version 0.2b Windows          ##")
+        Console.WriteLine("##            Version 0.4b Windows          ##")
         Console.WriteLine("##############################################")
         Console.WriteLine()
         Console.WriteLine("Programme a lancer :")
-        Console.WriteLine("----- Vecteurs -----")
+        Console.WriteLine("----- Vecteurs (2D) -----")
         Console.WriteLine("1. Triangle rectangle ou non ?")
         Console.WriteLine("2. Quadrilatère Parallélogramme ou non ?")
-        Console.WriteLine("3. Vecteurs Colinéaires ou non ?")
-        Console.WriteLine("4. Test de l'alignement de 3 points")
-        Console.WriteLine("5. Droite parallèle ou non ?")
-        Console.WriteLine("----- Fonctions Affines -----")
-        Console.WriteLine("6. Fonction croissance ou non ?")
-        Console.WriteLine("7. En quel point s'annule la fonction ?")
-        Console.WriteLine("8. Tarifs centre de vacances")
-        Console.WriteLine("9. Promotion du Magasin")
+        Console.WriteLine("3. Droite parallèle ou non ?")
+        Console.WriteLine("----- Vecteurs (3D) -----")
+        Console.WriteLine("4. Vecteurs Colinéaires ou non ?")
+        Console.WriteLine("5. Test de l'alignement de 3 points")
+        Console.WriteLine("6. Coordonnées d'un Vecteur")
+        Console.WriteLine("7. Distance d'un Vecteur")
+        Console.WriteLine("----- Système -----")
+        Console.WriteLine("A. A Propos")
         Console.WriteLine("Q. Sortir du programme")
         Console.Write("Entrez votre choix :")
         Choix = Console.ReadLine()
@@ -46,9 +57,10 @@ Menu:
         Console.Clear()
         Console.WriteLine("##############################################")
         Console.WriteLine("##          Maths ToolBox By VPARRES        ##")
-        Console.WriteLine("##            Version 0.2b Windows          ##")
+        Console.WriteLine("##            Version 0.4b Windows          ##")
         Console.WriteLine("##############################################")
         Console.WriteLine()
+        'Partie Vecteurs 2D
         Select Case Choix
             Case "1"
                 Console.WriteLine("Programme : Triangle rectangle ou non ?")
@@ -61,9 +73,9 @@ Menu:
                 Console.Write("Entrez C : ")
                 C = Console.ReadLine()
                 If C ^ 2 = A ^ 2 + B ^ 2 Then
-                    Console.WriteLine("Le triangle est rectangle")
+                    Console.WriteLine("Verdict : Le triangle est rectangle")
                 Else
-                    Console.WriteLine("Le triangle n'est pas rectangle")
+                    Console.WriteLine("Verdict : Le triangle n'est pas rectangle")
                 End If
             Case "2"
                 Console.WriteLine("Programme : Quadrilatère Parallélogramme ou non ?")
@@ -86,56 +98,12 @@ Menu:
                 Console.Write("Entrez Yd : ")
                 YD = Console.ReadLine()
                 If ((XA + XC) / 2) = ((XB + XD) / 2) And ((YA + YC) / 2) = ((YB + YC) / 2) Then
-                    Console.WriteLine("Le Quadrilatère est un parallélogramme")
+                    Console.WriteLine("Verdict : Le Quadrilatère est un parallélogramme")
                 Else
-                    Console.WriteLine("Le Quadrilatère n'est pas parallélogramme")
+                    Console.WriteLine("Verdict : Le Quadrilatère n'est pas parallélogramme")
                 End If
+
             Case "3"
-                Console.WriteLine("Programme : Vecteurs Colinéaires ou non ?")
-                Console.WriteLine("----------------------------------------")
-                Console.WriteLine()
-                Console.Write("Entrez X : ")
-                XA = Console.ReadLine()
-                Console.Write("Entrez Y : ")
-                YA = Console.ReadLine()
-                Console.Write("Entrez X' : ")
-                XB = Console.ReadLine()
-                Console.Write("Entrez Y' : ")
-                YB = Console.ReadLine()
-                If (XA * YB) - (YA * XB) = 0 Then
-                    Console.WriteLine("Vecteurs Colinéaire")
-                Else
-                    Console.WriteLine("Vecteurs Non Colinéaires")
-                End If
-            Case "4"
-                Console.WriteLine("Programme : Test de l'alignement de 3 points")
-                Console.WriteLine("--------------------------------------------")
-                Console.WriteLine()
-                Console.Write("Entrez Xa : ")
-                XA = Console.ReadLine()
-                Console.Write("Entrez Ya : ")
-                YA = Console.ReadLine()
-                Console.Write("Entrez Xb : ")
-                XB = Console.ReadLine()
-                Console.Write("Entrez Yb : ")
-                YB = Console.ReadLine()
-                Console.Write("Entrez Xc : ")
-                XC = Console.ReadLine()
-                Console.Write("Entrez Yc : ")
-                YC = Console.ReadLine()
-
-                XBA = XB - XA
-                YBA = YB - YA
-                XCB = XC - XB
-                YCB = YC - YB
-
-                If (YBA * XCB) = (YCB * XBA) Then
-                    Console.WriteLine("ABC sont alignés")
-                Else
-                    Console.WriteLine("ABC ne sont pas Alignés")
-                End If
-
-            Case "5"
                 Console.WriteLine("Programme : Droite parallèle ou non ?")
                 Console.WriteLine("------------------------------------")
                 Console.WriteLine()
@@ -157,12 +125,131 @@ Menu:
                 YD = Console.ReadLine()
 
                 If (XB - XA) * (YC - YD) - (YB - YA) * (XC - XD) = 0 Then
-                    Console.WriteLine("Les Droites sont Parralèles")
+                    Console.WriteLine("Verdict : Les Droites sont Parralèles")
                 Else
-                    Console.WriteLine("Les Droites ne sont pas parralèles")
+                    Console.WriteLine("Verdict : Les Droites ne sont pas parralèles")
                 End If
 
-            Case "6", "7", "8", "9"
+                'Partie Vecteur 3D
+            Case "4"
+                Console.WriteLine("Programme : Vecteurs Colinéaires ou non ?")
+                Console.WriteLine("----------------------------------------")
+                Console.WriteLine()
+                Console.Write("Entrez Xa : ")
+                XA = Console.ReadLine()
+                Console.Write("Entrez Ya : ")
+                YA = Console.ReadLine()
+                Console.Write("Entrez Za : ")
+                ZA = Console.ReadLine()
+                Console.Write("Entrez Xb : ")
+                XB = Console.ReadLine()
+                Console.Write("Entrez Yb : ")
+                YB = Console.ReadLine()
+                Console.Write("Entrez Zb : ")
+                ZB = Console.ReadLine()
+                If (XA * YB * ZA) - (YA * XB * ZB) = 0 Then
+                    Console.WriteLine("Verdict : Les Vecteurs sont Colinéaire")
+                Else
+                    Console.WriteLine("Verdict : Les Vecteurs ne sont pas Colinéaires")
+                End If
+            Case "5"
+                Console.WriteLine("Programme : Test de l'alignement de 3 points")
+                Console.WriteLine("-------------------------------------------")
+                Console.WriteLine()
+                Console.Write("Entrez Xa : ")
+                XA = Console.ReadLine()
+                Console.Write("Entrez Ya : ")
+                YA = Console.ReadLine()
+                Console.Write("Entrez Za : ")
+                ZA = Console.ReadLine()
+                Console.Write("Entrez Xb : ")
+                XB = Console.ReadLine()
+                Console.Write("Entrez Yb : ")
+                YB = Console.ReadLine()
+                Console.Write("Entrez Zb : ")
+                ZB = Console.ReadLine()
+                Console.Write("Entrez Xc : ")
+                XC = Console.ReadLine()
+                Console.Write("Entrez Yc : ")
+                YC = Console.ReadLine()
+                Console.Write("Entrez Zc : ")
+                ZC = Console.ReadLine()
+
+                XBA = XB - XA
+                YBA = YB - YA
+                ZBA = ZB - ZA
+                XAC = XC - XA
+                YAC = YC - YA
+                ZAC = ZC - ZA
+
+                If (XBA / XAC) = (YBA / YAC) = (ZBA / ZAC) Then
+                    Console.WriteLine("Verdict : ABC sont alignés")
+                Else
+                    Console.WriteLine("Verdict : ABC ne sont pas Alignés")
+                End If
+
+            Case "6"
+                Console.WriteLine("Programme : Coordonnées d'un Vecteur")
+                Console.WriteLine("-----------------------------------")
+                Console.WriteLine()
+                Console.Write("Entrez Xa : ")
+                XA = Console.ReadLine()
+                Console.Write("Entrez Ya : ")
+                YA = Console.ReadLine()
+                Console.Write("Entrez Za : ")
+                ZA = Console.ReadLine()
+                Console.Write("Entrez Xb : ")
+                XB = Console.ReadLine()
+                Console.Write("Entrez Yb : ")
+                YB = Console.ReadLine()
+                Console.Write("Entrez Zb : ")
+                ZB = Console.ReadLine()
+
+                XBA = XB - XA
+                YBA = YB - YA
+                ZBA = ZB - ZA
+
+                Console.WriteLine("Coordonnées du Vecteur :")
+                Console.WriteLine("Xab = " & XBA)
+                Console.WriteLine("Yab = " & YBA)
+                Console.WriteLine("Zab = " & ZBA)
+
+            Case "7"
+                Console.WriteLine("Programme : Distance d'un Vecteur")
+                Console.WriteLine("---------------------------------")
+                Console.WriteLine()
+                Console.Write("Entrez Xa : ")
+                XA = Console.ReadLine()
+                Console.Write("Entrez Ya : ")
+                YA = Console.ReadLine()
+                Console.Write("Entrez Za : ")
+                ZA = Console.ReadLine()
+                Console.Write("Entrez Xb : ")
+                XB = Console.ReadLine()
+                Console.Write("Entrez Yb : ")
+                YB = Console.ReadLine()
+                Console.Write("Entrez Zb : ")
+                ZB = Console.ReadLine()
+
+                XBA = XB - XA
+                YBA = YB - YA
+                ZBA = ZB - ZA
+                A = Pow(XBA, 2) + Pow(YBA, 2) + Pow(ZBA, 2)
+                B = Sqrt(A)
+
+                Console.WriteLine("Distance du Vecteur :")
+                Console.WriteLine("AB² = " & A)
+                Console.WriteLine("√(AB) = " & B)
+                'Partie Système
+            Case "A"
+                Console.WriteLine("A Propos ...")
+                Console.WriteLine("------------------------------------")
+                Console.WriteLine()
+                Console.WriteLine("Maths ToolBox")
+                Console.WriteLine("Version : 0.4 Beta")
+                Console.WriteLine("Auteur : Vincent PARRES (vparres)")
+                Console.WriteLine("Codé en VB.net, .Net Framework 4.5")
+            Case "8", "9"
                 Console.WriteLine("/!\ Erreur")
                 Console.WriteLine("------------------------------------")
                 Console.WriteLine("Fonction non implentée !")
--- README.md
+++ README.md
@@ -5,5 +5,4 @@ MathsToolBox est une petite application codée en Visual Basic.net qui a été i
 de Mathématiques de 2nd et elle permet de faire des calculs divers avec des vecteurs et des fonctions. Cette application est
 modifiable librement et est distribuée sous license GPL v3.
 
-(!) Le developpement de cette application est inachevé pour l'instant : La partie calcul avec des fonctions n'est pas encore 
-developpée.
+(!) Le developpement de cette application est inachevé pour l'instant : De nouvelles méthodes de calcul vont êtres implantées
